8f45993de134c9846c32b3f896a5bad8d76b230a
pascal <p.jipp87@gmail.com>
2021-12-11 23:57:17 +0100

    .

---
+++ Components/Navigation.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import InfoScreen from "../Screens/InfoScreen";
+import MainScreen from "../Screens/MainScreen";
+import SettingsScreen from "../Screens/SettingsScreen";
+import * as Icon from "@expo/vector-icons";
+
+const Tab = createBottomTabNavigator();
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Startseite") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "Einstellungen") {
+              iconName = focused ? "settings" : "settings-outline";
+            } else if (route.name === "Info") {
+              iconName = focused
+                ? "information-circle"
+                : "information-circle-outline";
+            }
+
+            // You can return any component that you like here!
+            return <Icon.Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Startseite" component={MainScreen} />
+        <Tab.Screen name="Einstellungen" component={SettingsScreen} />
+        <Tab.Screen name="Info" component={InfoScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({});
